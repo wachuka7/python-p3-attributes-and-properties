1cd63f8dcefa948d9221c6676943f3122c1e4d49
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-07 08:19:52 +0300

    feat: add name and breed setters

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +10,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='', breed=''):
+        self._name = None  # Initialize the name attribute
+        self.name = name   # Use the setter method to set the name
+        self.breed= None
+        self.breed=breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not 1 <= len(value) <= 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+        
+        @property
+        def breed(self):
+            return self._breed
+
+        @breed.setter
+        def breed(self, new_breed):
+            # Check if the new breed is in the list of approved breeds
+            if new_breed in APPROVED_BREEDS:
+                self._breed = new_breed
+            else:
+                print("Breed must be in list of approved breeds.")
